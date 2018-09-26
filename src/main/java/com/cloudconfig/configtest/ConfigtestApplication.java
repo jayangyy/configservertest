5d229f4bbf277fa7ad3f7929d5f4e3d9197c2c62
Jayang <Jayang@DESKTOP-FTGA4BL>
2018-09-26 14:54:15 +0800

    禅道库并入系统

---
+++ src/main/java/com/cloudconfig/configtest/ConfigtestApplication.java
@@ -0,0 +1,12 @@
+package com.cloudconfig.configtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigtestApplication.class, args);
+	}
+}
