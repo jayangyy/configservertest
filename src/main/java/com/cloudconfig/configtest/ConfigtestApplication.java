1420b28b3deeafc98a511b2eda11d25b01e78018
Jayang <Jayang@DESKTOP-FTGA4BL>
2018-09-26 15:02:25 +0800

    禅道库并入系统

--- src/main/java/com/cloudconfig/configtest/ConfigtestApplication.java
+++ src/main/java/com/cloudconfig/configtest/ConfigtestApplication.java
@@ -2,7 +2,8 @@ package com.cloudconfig.configtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigtestApplication {
 
